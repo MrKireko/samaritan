e278cab7080a083724be2a492f5e33fb8d6577a8
mrkireko <mrkireko+cxmio@gmail.com>
2015-08-29 16:45:51 +0200

    Better animation

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var url = document.URL
 	var i = 0;
 	if (url.indexOf("?") > -1){
-		var inp = url.replace("?", "_").replace("%20", "_").split("_");
+		var inp = url.replace("?", "_").split("_");
 		i = 1;
 	}
 	inp.push(inp[inp.length - 1])
@@ -13,6 +13,8 @@ $(document).ready(function(){
 			$("#txt").css("color", "black");
 			$("#txt").html(inp[i]);
 			i = i+1;
+			$("#line").css("width", $("#txt").css("width"));
+			console.log($("#txt").css("width"));
 			if (i == inp.length) {
 				$("#txt").css("color", "rgba(0, 0, 0, 0)");
 				setTimeout(function(){ $("#txt").html("---"); }, 500);
